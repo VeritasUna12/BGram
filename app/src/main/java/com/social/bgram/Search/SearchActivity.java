c28337a4b38db2adf0f6f0cc734b939c45da4a20
Eng.binrashad <bin.rashad@gmail.com>
2018-01-07 22:05:38 +0300

    init_add_registar_activity

--- app/src/main/java/com/social/bgram/Search/SearchActivity.java
+++ app/src/main/java/com/social/bgram/Search/SearchActivity.java
@@ -39,7 +39,7 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         Log.d(TAG, "onCreate: started.");
 
         setUpBottomNavigationView();
