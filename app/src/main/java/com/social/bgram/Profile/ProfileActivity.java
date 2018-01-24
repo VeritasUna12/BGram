336f2289850470d9cdf00990d3fa958ed22eb46f
engmnmubarak <eng.m.n.mubarak@gmail.com>
2018-01-25 01:56:52 +0300

    Merge branch 'master' of C:\Users\mubarak\Documents\GitHub\BGram with conflicts.

--- app/src/main/java/com/social/bgram/Profile/ProfileActivity.java
+++ app/src/main/java/com/social/bgram/Profile/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.social.bgram.Profile;
 
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -28,11 +29,10 @@ import com.social.bgram.Search.SearchActivity;
 public class ProfileActivity extends AppCompatActivity {
 
     private static final String TAG = "ProfileActivity";
-
     private Context mContext= ProfileActivity.this;
     private ProgressBar mProgressBar;
 
-    private TextView text_edit_profile;
+//    private TextView text_edit_profile;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,154 +44,164 @@ public class ProfileActivity extends AppCompatActivity {
         ProgressBar mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
         mProgressBar.setVisibility(View.GONE);
 
-        circularFloatingActionMenu();
-        setupToolbar();
-        text_edit_profile = (TextView)findViewById(R.id.textEditProfile);
+        init();
 
-        text_edit_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+//        circularFloatingActionMenu();
+//        setupToolbar();
+//        text_edit_profile = (TextView)findViewById(R.id.textEditProfile);
+//
+//        text_edit_profile.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
     }
 
-    private void setupToolbar(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolbar);
-        setSupportActionBar(toolbar);
-
-        // implement when click on menu button
-        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
-        profileMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: navigating to account settings.");
-                Intent intent = new Intent(mContext, AccountSettingActivity.class);
-                startActivity(intent);
-            }
-        });
-
+    private void init(){
+        ProfileFragment fragment = new ProfileFragment();
+        android.support.v4.app.FragmentTransaction transaction = ProfileActivity.this.getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container,fragment);
+        transaction.addToBackStack(getString(R.string.profile_fragment));
+        transaction.commit();
     }
+
+//    private void setupToolbar(){
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolbar);
+//        setSupportActionBar(toolbar);
+//
+//        // implement when click on menu button
+//        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
+//        profileMenu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: navigating to account settings.");
+//                Intent intent = new Intent(mContext, AccountSettingActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//    }
 
    /*
      ***********************************************************************************************
      */
 
-    /*
-     *********************************** Floating Action Menu **************************************
-     */
-
-    // circularFloatingActionMenu Setup
-    private void circularFloatingActionMenu() {
-
-        final ImageView icon = new ImageView(this); // Create an icon
-        icon.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_launch_black_24dp));
-
-        final FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
-                .setContentView(icon)
-                .setPosition(FloatingActionButton.POSITION_BOTTOM_RIGHT)
-                .build();
-
-        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
-        ImageView homeIcon          = new ImageView(this);
-        ImageView searchIcon        = new ImageView(this);
-        ImageView addIcon           = new ImageView(this);
-        ImageView profileIcon       = new ImageView(this);
-        ImageView notificationIcon  = new ImageView(this);
-
-        homeIcon            .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_home) );
-        profileIcon         .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_profile) );
-        notificationIcon    .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_notification) );
-        addIcon             .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_add) );
-        searchIcon          .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_search) );
-
-        SubActionButton homeButton = itemBuilder.setContentView(homeIcon).build();
-        SubActionButton searchButton = itemBuilder.setContentView(searchIcon).build();
-        SubActionButton addButton = itemBuilder.setContentView(addIcon).build();
-        SubActionButton profileButton = itemBuilder.setContentView(profileIcon).build();
-        SubActionButton notificationButton = itemBuilder.setContentView(notificationIcon).build();
-
-        //show icons in Floating Action Menu
-        final FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
-                .addSubActionView(profileButton)
-                .addSubActionView(notificationButton)
-                .addSubActionView(addButton)
-                .addSubActionView(searchButton)
-                .addSubActionView(homeButton)
-                // ...
-                .attachTo(actionButton)
-                .build();
-
-        actionMenu.setStateChangeListener(new FloatingActionMenu.MenuStateChangeListener() {
-            @Override
-            public void onMenuOpened(FloatingActionMenu floatingActionMenu) {
-                icon.setRotation(0);
-                PropertyValuesHolder pvhr = PropertyValuesHolder.ofFloat(View.ROTATION,45);
-                ObjectAnimator animation = ObjectAnimator.ofPropertyValuesHolder(icon, pvhr);
-                animation.start();
-            }
-
-            @Override
-            public void onMenuClosed(FloatingActionMenu floatingActionMenu) {
-                icon.setRotation(45);
-                PropertyValuesHolder pvhr = PropertyValuesHolder.ofFloat(View.ROTATION,0);
-                ObjectAnimator animation = ObjectAnimator.ofPropertyValuesHolder(icon, pvhr);
-                animation.start();
-            }
-        });
-        homeIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "HOME", Toast.LENGTH_SHORT).show();
-                Intent hometnt = new Intent(mContext, HomeActivity.class);
-                startActivity(hometnt);
-                finish();
-            }
-        });
-
-        searchIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "SEARCH", Toast.LENGTH_SHORT).show();
-                Intent searchtnt = new Intent(mContext, SearchActivity.class);
-                startActivity(searchtnt);
-                finish();
-            }
-        });
-
-        addIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "ADD", Toast.LENGTH_SHORT).show();
-                Intent addtnt = new Intent(mContext, AddActivity.class);
-                startActivity(addtnt);
-                finish();
-            }
-        });
-
-        notificationIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "NOTIFICATION", Toast.LENGTH_SHORT).show();
-                Intent notificationtnt = new Intent(mContext, NotificationActivity.class);
-                startActivity(notificationtnt);
-                finish();
-            }
-        });
-
-        profileIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "PROFILE", Toast.LENGTH_SHORT).show();
-                Intent profiletnt = new Intent(mContext, ProfileActivity.class);
-                startActivity(profiletnt);
-                finish();
-            }
-        });
-    }
-
-    /*
-     ***********************************************************************************************
-     */
+//    /*
+//     *********************************** Floating Action Menu **************************************
+//     */
+//
+//    // BottomNavigationView Setup
+//    private void circularFloatingActionMenu() {
+//
+//        final ImageView icon = new ImageView(this); // Create an icon
+//        icon.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_launch_black_24dp));
+//
+//        final FloatingActionButton actionButton = new FloatingActionButton.Builder(this)
+//                .setContentView(icon)
+//                .setPosition(FloatingActionButton.POSITION_BOTTOM_RIGHT)
+//                .build();
+//
+//        SubActionButton.Builder itemBuilder = new SubActionButton.Builder(this);
+//        ImageView homeIcon          = new ImageView(this);
+//        ImageView searchIcon        = new ImageView(this);
+//        ImageView addIcon           = new ImageView(this);
+//        ImageView profileIcon       = new ImageView(this);
+//        ImageView notificationIcon  = new ImageView(this);
+//
+//        homeIcon            .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_home) );
+//        profileIcon         .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_profile) );
+//        notificationIcon    .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_notification) );
+//        addIcon             .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_add) );
+//        searchIcon          .setImageDrawable( ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_search) );
+//
+//        SubActionButton homeButton = itemBuilder.setContentView(homeIcon).build();
+//        SubActionButton searchButton = itemBuilder.setContentView(searchIcon).build();
+//        SubActionButton addButton = itemBuilder.setContentView(addIcon).build();
+//        SubActionButton profileButton = itemBuilder.setContentView(profileIcon).build();
+//        SubActionButton notificationButton = itemBuilder.setContentView(notificationIcon).build();
+//
+//        //show icons in Floating Action Menu
+//        final FloatingActionMenu actionMenu = new FloatingActionMenu.Builder(this)
+//                .addSubActionView(profileButton)
+//                .addSubActionView(notificationButton)
+//                .addSubActionView(addButton)
+//                .addSubActionView(searchButton)
+//                .addSubActionView(homeButton)
+//                // ...
+//                .attachTo(actionButton)
+//                .build();
+//
+//        actionMenu.setStateChangeListener(new FloatingActionMenu.MenuStateChangeListener() {
+//            @Override
+//            public void onMenuOpened(FloatingActionMenu floatingActionMenu) {
+//                icon.setRotation(0);
+//                PropertyValuesHolder pvhr = PropertyValuesHolder.ofFloat(View.ROTATION,45);
+//                ObjectAnimator animation = ObjectAnimator.ofPropertyValuesHolder(icon, pvhr);
+//                animation.start();
+//            }
+//
+//            @Override
+//            public void onMenuClosed(FloatingActionMenu floatingActionMenu) {
+//                icon.setRotation(45);
+//                PropertyValuesHolder pvhr = PropertyValuesHolder.ofFloat(View.ROTATION,0);
+//                ObjectAnimator animation = ObjectAnimator.ofPropertyValuesHolder(icon, pvhr);
+//                animation.start();
+//            }
+//        });
+//        homeIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "HOME", Toast.LENGTH_SHORT).show();
+//                Intent hometnt = new Intent(mContext, HomeActivity.class);
+//                startActivity(hometnt);
+//                finish();
+//            }
+//        });
+//
+//        searchIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "SEARCH", Toast.LENGTH_SHORT).show();
+//                Intent searchtnt = new Intent(mContext, SearchActivity.class);
+//                startActivity(searchtnt);
+//                finish();
+//            }
+//        });
+//
+//        addIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "ADD", Toast.LENGTH_SHORT).show();
+//                Intent addtnt = new Intent(mContext, AddActivity.class);
+//                startActivity(addtnt);
+//                finish();
+//            }
+//        });
+//
+//        notificationIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "NOTIFICATION", Toast.LENGTH_SHORT).show();
+//                Intent notificationtnt = new Intent(mContext, NotificationActivity.class);
+//                startActivity(notificationtnt);
+//                finish();
+//            }
+//        });
+//
+//        profileIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(), "PROFILE", Toast.LENGTH_SHORT).show();
+//                Intent profiletnt = new Intent(mContext, ProfileActivity.class);
+//                startActivity(profiletnt);
+//                finish();
+//            }
+//        });
+//    }
+//
+//    /*
+//     ***********************************************************************************************
+//     */
 
 }
